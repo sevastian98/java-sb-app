6e69283287818082fa439e226c9016e922c1027a
Sevastian Vascan <sevastian.vascan7@gmail.com>
2025-05-17 13:38:24 +0300

    Initial commit

---
+++ src/main/java/java_sb_app/sb_app/SbAppApplication.java
@@ -0,0 +1,13 @@
+package java_sb_app.sb_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java_sb_app/sb_app/SbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package java_sb_app.sb_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
